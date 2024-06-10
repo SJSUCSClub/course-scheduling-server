699da52ef466edf1be26106873fb856d24fbb6e3
Justin <justlee888@gmail.com>
2024-06-10 00:54:38 -0700

    adds user to db when logged in, and redirects to frontend with the cookies.

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.urls import reverse
+import http.client
+import json
+from django.contrib.auth.models import User
+from django.contrib.auth import login
 import google.oauth2.credentials
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
@@ -19,25 +23,52 @@ def credentials_to_dict(credentials):
 #add user to db
 #send and create ID token from backend containing user's profile pic,name, etc httponly false so browser can access it
 def GoogleAuthorize(request):
-    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(CLIENT_SECRETS_FILE, scopes=SCOPES)
-    flow.redirect_uri = request.build_absolute_uri(reverse('oauth2callback'))
-    authorization_url, state = flow.authorization_url(
-    access_type='offline',
-    include_granted_scopes='true')
-    request.session['state'] = state
-    return redirect(authorization_url)
+  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(CLIENT_SECRETS_FILE, scopes=SCOPES)
+  flow.redirect_uri = request.build_absolute_uri(reverse('oauth2callback'))
+  authorization_url, state = flow.authorization_url(
+  access_type='offline',
+  include_granted_scopes='true')
+  request.session['state'] = state
+  return redirect(authorization_url)
 
 def oauth2callback(request):
-    state = request.session['state']
-    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-      CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
-    flow.redirect_uri = request.build_absolute_uri(reverse('oauth2callback'))
-    authorization_response = request.build_absolute_uri()
-    flow.fetch_token(authorization_response=authorization_response)
-    credentials = flow.credentials
-    request.session['credentials'] = credentials_to_dict(credentials)
-    response = HttpResponse('blah')
-    response.set_cookie('token', credentials.token, httponly=True)
-    response.set_cookie('refresh_token', credentials.refresh_token, httponly=True)
-    return response
+  state = request.session['state']
+  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+    CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
+  flow.redirect_uri = request.build_absolute_uri(reverse('oauth2callback'))
+  authorization_response = request.build_absolute_uri()
+  flow.fetch_token(authorization_response=authorization_response)
+  credentials = flow.credentials
+  request.session['credentials'] = credentials_to_dict(credentials)
+  print(request.session['credentials'])
+  conn = http.client.HTTPSConnection("www.googleapis.com")
+  headers = {'Authorization': f'Bearer {credentials.token}'}
+  conn.request("GET", "/oauth2/v2/userinfo", headers=headers)
+  google_response = conn.getresponse()
 
+  user_info = json.loads(google_response.read().decode())
+  email = user_info.get('email')
+  first_name = user_info.get('given_name')
+  last_name = user_info.get('family_name')
+  print(email, first_name, last_name)
+  user, created = User.objects.get_or_create(
+    email=email,
+    defaults={'username': email, 'first_name': first_name, 'last_name': last_name}
+  )
+  if created:
+    #unusuable password bc we're using oauth
+    user.set_unusable_password()
+    user.save()
+  user_data = {
+    'email': email,
+    'first_name': first_name,
+    'last_name': last_name
+  }
+  login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+  response = HttpResponse('blah')
+  response.set_cookie('token', credentials.token, httponly=True)
+  response.set_cookie('refresh_token', credentials.refresh_token, httponly=True)
+  response.set_cookie('user_data', json.dumps(user_data))
+  response['Location'] = 'http://localhost:3000'
+  response.status_code = 302
+  return response
