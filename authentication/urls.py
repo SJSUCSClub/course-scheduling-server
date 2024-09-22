53ec69bcc8fdaf000ff4a81558a927fe11bd1954
ahmadgaz <gazali.contact@gmail.com>
2024-09-22 11:38:52 -0700

    removed trailing slashes

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,8 +5,8 @@ from authentication.views import GoogleLogin
 from dj_rest_auth.jwt_auth import get_refresh_view
 
 urlpatterns = [
-    path("user/", UserDetailsView.as_view(), name="rest_user_details"),
-    path("google/", GoogleLogin.as_view(), name="google_login"),
-    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
-    path("token/refresh/", get_refresh_view().as_view(), name="token_refresh"),
+    path("user", UserDetailsView.as_view(), name="rest_user_details"),
+    path("google", GoogleLogin.as_view(), name="google_login"),
+    path("token/verify", TokenVerifyView.as_view(), name="token_verify"),
+    path("token/refresh", get_refresh_view().as_view(), name="token_refresh"),
 ]
