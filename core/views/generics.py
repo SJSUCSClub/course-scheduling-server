20d15d24fa4c0a6f27faa3b81f3990975915d2b4
Karthik Manishankar <emailclub72@gmail.com>
2024-09-26 21:36:49 -0700

    Fixed Trailing Slash + Clarification on JSON Response

--- core/views/generics.py
+++ core/views/generics.py
@@ -13,24 +13,24 @@ def health_check(request):
     start_time = time.time()
     try:
         professor_select_summary("david.taylor")
-        db_status = "healthy"
+        db_status = "Healthy"
     except OperationalError:
-        db_status = "unhealthy"
+        db_status = "Unhealthy"
     end_time = time.time()
 
     # Database Status
     db_check_time = end_time - start_time
     if db_check_time > db_timeout:
-        db_status = "unhealthy"
+        db_status = "Unhealthy"
 
     # Overall Status
-    overall_status = "healthy" if db_status == "healthy" else "unhealthy"
+    overall_status = "Healthy" if db_status == "Healthy" else "Unhealthy"
     response_data = {
-        "status": overall_status,
-        "database": {
-            "status": db_status,
-            "time": db_check_time
+        "Status": overall_status,
+        "Database": {
+            "Status": db_status,
+            "Response_time": db_check_time
         },
     }
-    status_code = 200 if overall_status == "healthy" else 503
+    status_code = 200 if overall_status == "Healthy" else 503
     return JsonResponse(response_data, status=status_code)
