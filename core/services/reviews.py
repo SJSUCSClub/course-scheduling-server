d6e5630a6e78fbb401485ba40900644851155846
chrehall68 <chrehall68@gmail.com>
2024-10-13 19:04:24 -0700

    set reviewer_name to None when anonymous

--- core/services/reviews.py
+++ core/services/reviews.py
@@ -4,8 +4,9 @@ from core.daos import (
     review_select_upvotes,
     review_select_tags,
     review_select_comments,
-    user_voted_review
+    user_voted_review,
 )
+
 
 def get_paginated_reviews_by_course(
     dept: str, course_number: str, limit: int, page: int, tags=[], user_id=None
@@ -21,10 +22,11 @@ def get_paginated_reviews_by_course(
     for review in reviews:
         votes = review_select_upvotes(review["id"])
         review["votes"] = votes
-        review["user_vote"] = user_voted_review(
-            user_id=user_id,
-            review_id=review["id"]
-        )
+        review["user_vote"] = user_voted_review(user_id=user_id, review_id=review["id"])
+
+        # don't send user's actual name if the review is anonymous
+        if review["is_user_anonymous"]:
+            review["reviewer_name"] = None
 
     total_results = reviews_select_count(dept, course_number, tags=tags)
     tags = review_select_tags(department=dept, course_number=course_number, tags=tags)
@@ -57,10 +59,11 @@ def get_paginated_reviews_by_professor(
         votes = review_select_upvotes(review["id"])
         review["votes"] = votes
         review["comments"] = review_select_comments(review["id"])
-        review["user_vote"] = user_voted_review(
-            user_id=user_id,
-            review_id=review["id"]
-        )
+        review["user_vote"] = user_voted_review(user_id=user_id, review_id=review["id"])
+
+        # don't send user's actual name if the review is anonymous
+        if review["is_user_anonymous"]:
+            review["reviewer_name"] = None
 
     total_results = reviews_select_count(professor_id=professor_id, tags=tags)
     tags = review_select_tags(professor_id=professor_id, tags=tags)
@@ -73,8 +76,10 @@ def get_paginated_reviews_by_professor(
         "filters": {"tags": tags},
     }
 
+
 def get_review_comments(review_id: str):
     return review_select_comments(review_id)
+
 
 def get_paginated_reviews_with_comments_by_course(
     dept, course_number, limit, page, tags=[]
